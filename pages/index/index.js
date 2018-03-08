030eb6cbcf9958dd5f4d339b197e190279f35791
luobing <luobing@70a58d25-dd47-4253-b6d4-9be2152c5396>
2018-03-08 14:22:59 +0000

    Init project
    git-svn-id: svn://svn.konggeek.com/code/wechat/d9gym/trunk@42829 70a58d25-dd47-4253-b6d4-9be2152c5396

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,39 +16,50 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    var that = this;
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log("request code:" + res.code)
+        wx.request({
+          url: app.apiServerURL + "/user/wechatXcxLogin.htm",
+          data: {
+            version: app.version,
+            "code": res.code
+          },
+          success: function (res) {
+            console.log(res.data)
+            if (res.data.retCode != '0') {
+              if (res.data.retCode == '30006') {
+                wx.redirectTo({
+                  url: '/pages/frozen/frozen',
+                })
+              } else {
+                util.errorMsg(that, res);
+              }
+              return;
+            }
+            var user = res.data.data.user;
+            var accessToken = user.accessToken;
+            console.log('accessToken before:' + accessToken)
+            wx.setStorageSync('accessToken', accessToken);
+            console.log('accessToken after:' + wx.getStorageSync("accessToken"))
+            that.setData({
+              user: user
+            })
+            // 获取用户信息
+            app.requestAndUpdateUserInfo();
+            //加载第一页数据
+            // ajaxPage(that, 0);
+          }
+        })
+      },
+      fail: function (res) {
+        console.log(res)
+        wx.showToast({
+          title: '登录小程序失败',
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
-  }
+  },
 })
